8c36515bb58d93ad126332104e791dd2fc906f6b
Roberto Alagemovits <robertomovits@gmail.com>
2023-07-26 04:26:11 -0300

    hice una pagina de verificado y no verificado

--- Ej1 (Mejora de Ej1 If)/script.js
+++ Ej1 (Mejora de Ej1 If)/script.js
@@ -1,7 +1,7 @@
 const inputUsuario = document.querySelector("#usuario-nombre")
 const inputContrasena = document.querySelector("#usuario-contrasena")
 const pResultado = document.querySelector("#resultado")
-const contentedorDiv = document.querySelector("#div-contenedor");
+const resultadoDiv = document.querySelector("#resultados");
 
 const Usuarios = ["Roberto","Juan"];
 const Contrasenas = ["Roberto1","juanito"];
@@ -16,12 +16,19 @@ function verificar(){
     usuarioNombreIngresado = inputUsuario.value;
     usuarioContrasenaIngresada = inputContrasena.value;
     
+    resultadoDiv.innerHTML = ``;
+
     if (((usuarioNombreIngresado == Usuarios[0]) && (usuarioContrasenaIngresada == Contrasenas[0])) ) {
         pResultado.innerHTML = "Bienvenido " + Usuarios[0];
         pResultado.style.color = "green";
         imgRes1.src = "https://i.ebayimg.com/images/g/GbkAAOSwp2dd1oZ2/s-l1200.jpg";
         imgRes1.style.display = "block";
         imgRes2.style.display = "none";
+
+        resultadoDiv.innerHTML = `
+        <img id="img-verificada" src="${imgRes1.src}" alt="">
+        <p id="resultado" style="color:${pResultado.style.color}">${pResultado.innerHTML}</p>
+        `;
         
     } else if (((usuarioNombreIngresado == Usuarios[1]) && (usuarioContrasenaIngresada == Contrasenas[1]))) {
         pResultado.innerHTML = "Bienvenido " + Usuarios[1];
@@ -29,6 +36,11 @@ function verificar(){
         imgRes1.src = "https://i.ebayimg.com/images/g/GbkAAOSwp2dd1oZ2/s-l1200.jpg";
         imgRes1.style.display = "block";
         imgRes2.style.display = "none";
+
+        resultadoDiv.innerHTML = `
+        <img id="img-verificada" src="${imgRes1.src}" alt="">
+        <p id="resultado" style="color:${pResultado.style.color}">${pResultado.innerHTML}</p>
+        `;
     }
     
     else{
@@ -37,5 +49,10 @@ function verificar(){
         imgRes2.src = "https://clipart-library.com/data_images/49138.png";
         imgRes2.style.display = "block";
         imgRes1.style.display = "none";
+
+        resultadoDiv.innerHTML = `
+        <img id="img-no-verificada" src="${imgRes2.src}" alt="">
+        <p id="resultado" style="color:${pResultado.style.color}">${pResultado.innerHTML}</p>
+        `;
     }
 }
