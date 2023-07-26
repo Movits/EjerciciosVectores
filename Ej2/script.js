e75b49754c50b20ec9295036c37ee1288cd7b57d
Roberto Alagemovits <robertomovits@gmail.com>
2023-07-26 18:01:36 -0300

    primer commit

---
+++ Ej2/script.js
@@ -0,0 +1,13 @@
+const Productos = ["Producto 1", "Producto 2", "Producto 3"];
+const Stock = [100,100,100];
+
+function compra() {
+    switch (key) {
+        case value:
+            
+            break;
+    
+        default:
+            break;
+    }
+}
