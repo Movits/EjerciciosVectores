6560af34ae212fa73c5e210bcddb5f519d4c80b9
Roberto Alagemovits <robertomovits@gmail.com>
2023-07-26 04:43:32 -0300

    agregue 2 usuarios

--- Ej1 (Mejora de Ej1 If)/script.js
+++ Ej1 (Mejora de Ej1 If)/script.js
@@ -3,8 +3,8 @@ const inputContrasena = document.querySelector("#usuario-contrasena")
 const pResultado = document.querySelector("#resultado")
 const resultadoDiv = document.querySelector("#resultados");
 
-const Usuarios = ["Roberto","Juan"];
-const Contrasenas = ["Roberto1","juanito"];
+const Usuarios = ["Roberto","Juan","a","b"];
+const Contrasenas = ["Roberto1","juanito","1","2"];
 
 const imgRes1 = document.querySelector("#img-verificada");
 const imgRes2 = document.querySelector("#img-no-verificada");
@@ -44,9 +44,35 @@ function verificar(){
         <p id="resultado" style="color:${pResultado.style.color}">${pResultado.innerHTML}</p>
         `;
     }
+
+    else if (((usuarioNombreIngresado == Usuarios[2]) && (usuarioContrasenaIngresada == Contrasenas[2]))) {
+        pResultado.innerHTML = "Bienvenido " + Usuarios[2];
+        pResultado.style.color = "green";
+        imgRes1.src = "https://i.ebayimg.com/images/g/GbkAAOSwp2dd1oZ2/s-l1200.jpg";
+        imgRes1.style.display = "block";
+        imgRes2.style.display = "none";
+
+        resultadoDiv.innerHTML = `
+        <img id="img-verificada" src="${imgRes1.src}" alt="">
+        <p id="resultado" style="color:${pResultado.style.color}">${pResultado.innerHTML}</p>
+        `;
+    }
+    
+    else if (((usuarioNombreIngresado == Usuarios[3]) && (usuarioContrasenaIngresada == Contrasenas[3]))) {
+        pResultado.innerHTML = "Bienvenido " + Usuarios[3];
+        pResultado.style.color = "green";
+        imgRes1.src = "https://i.ebayimg.com/images/g/GbkAAOSwp2dd1oZ2/s-l1200.jpg";
+        imgRes1.style.display = "block";
+        imgRes2.style.display = "none";
+
+        resultadoDiv.innerHTML = `
+        <img id="img-verificada" src="${imgRes1.src}" alt="">
+        <p id="resultado" style="color:${pResultado.style.color}">${pResultado.innerHTML}</p>
+        `;
+    }
     
     else{
-        pResultado.innerHTML = "Usuario y contrasena incorrectos";
+        pResultado.innerHTML = "Usuario o contrasena incorrectos";
         pResultado.style.color = "red";
         imgRes2.src = "https://clipart-library.com/data_images/49138.png";
         imgRes2.style.display = "block";
