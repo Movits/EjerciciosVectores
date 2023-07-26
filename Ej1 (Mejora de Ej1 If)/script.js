93370f99b0b862a007a15d0ba7819f480578dc3e
Roberto Alagemovits <robertomovits@gmail.com>
2023-07-26 04:46:56 -0300

    agregue 3 usuarios nuevos

--- Ej1 (Mejora de Ej1 If)/script.js
+++ Ej1 (Mejora de Ej1 If)/script.js
@@ -3,8 +3,8 @@ const inputContrasena = document.querySelector("#usuario-contrasena")
 const pResultado = document.querySelector("#resultado")
 const resultadoDiv = document.querySelector("#resultados");
 
-const Usuarios = ["Roberto","Juan","a","b"];
-const Contrasenas = ["Roberto1","juanito","1","2"];
+const Usuarios = ["Roberto","Juan","a","b","c","d","e"];
+const Contrasenas = ["Roberto1","juanito","1","2","3","4","5"];
 
 const imgRes1 = document.querySelector("#img-verificada");
 const imgRes2 = document.querySelector("#img-no-verificada");
@@ -60,6 +60,45 @@ function verificar(){
     
     else if (((usuarioNombreIngresado == Usuarios[3]) && (usuarioContrasenaIngresada == Contrasenas[3]))) {
         pResultado.innerHTML = "Bienvenido " + Usuarios[3];
+        pResultado.style.color = "green";
+        imgRes1.src = "https://i.ebayimg.com/images/g/GbkAAOSwp2dd1oZ2/s-l1200.jpg";
+        imgRes1.style.display = "block";
+        imgRes2.style.display = "none";
+
+        resultadoDiv.innerHTML = `
+        <img id="img-verificada" src="${imgRes1.src}" alt="">
+        <p id="resultado" style="color:${pResultado.style.color}">${pResultado.innerHTML}</p>
+        `;
+    }
+
+    else if (((usuarioNombreIngresado == Usuarios[4]) && (usuarioContrasenaIngresada == Contrasenas[4]))) {
+        pResultado.innerHTML = "Bienvenido " + Usuarios[4];
+        pResultado.style.color = "green";
+        imgRes1.src = "https://i.ebayimg.com/images/g/GbkAAOSwp2dd1oZ2/s-l1200.jpg";
+        imgRes1.style.display = "block";
+        imgRes2.style.display = "none";
+
+        resultadoDiv.innerHTML = `
+        <img id="img-verificada" src="${imgRes1.src}" alt="">
+        <p id="resultado" style="color:${pResultado.style.color}">${pResultado.innerHTML}</p>
+        `;
+    }
+
+    else if (((usuarioNombreIngresado == Usuarios[5]) && (usuarioContrasenaIngresada == Contrasenas[5]))) {
+        pResultado.innerHTML = "Bienvenido " + Usuarios[5];
+        pResultado.style.color = "green";
+        imgRes1.src = "https://i.ebayimg.com/images/g/GbkAAOSwp2dd1oZ2/s-l1200.jpg";
+        imgRes1.style.display = "block";
+        imgRes2.style.display = "none";
+
+        resultadoDiv.innerHTML = `
+        <img id="img-verificada" src="${imgRes1.src}" alt="">
+        <p id="resultado" style="color:${pResultado.style.color}">${pResultado.innerHTML}</p>
+        `;
+    }
+
+    else if (((usuarioNombreIngresado == Usuarios[6]) && (usuarioContrasenaIngresada == Contrasenas[6]))) {
+        pResultado.innerHTML = "Bienvenido " + Usuarios[6];
         pResultado.style.color = "green";
         imgRes1.src = "https://i.ebayimg.com/images/g/GbkAAOSwp2dd1oZ2/s-l1200.jpg";
         imgRes1.style.display = "block";
