518dd70a4bf0f72881798db4929bac2505d13dd8
Roberto Alagemovits <robertomovits@gmail.com>
2023-07-27 18:31:23 -0300

    agregue una funcion generica para actualizar conteudo en html

--- Ej3/script.js
+++ Ej3/script.js
@@ -1,5 +1,6 @@
 const inputImgs = document.querySelector("#input-imgs");
 const divImgs = document.querySelector("#div-imgs");
+const inputPosImg = document.querySelector("#pos-img");
 
 const IMGS = [];
 
@@ -9,4 +10,24 @@ function agregar() {
     divImgs.innerHTML += `
         <img src="${IMGS_AGREGADAS}" alt="">
     `;
+}
+
+function borrarUltImg() {
+    IMGS.pop();
+}
+
+const posImg = inputPosImg.value;
+
+function borrarImgPos(posImg) {
+    
+}
+
+function updateDivImgs() {
+    let htmlConteudo = ``;
+    for (let i = 0; i < IMGS.length; i++) {
+        htmlConteudo += `
+            <img src="${IMGS[i]}" alt="">
+        `;
+    }
+    divImgs.innerHTML = htmlConteudo;
 }
