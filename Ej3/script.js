ab1eb02669aad90f98de353e3225aedbfa11202d
Roberto Alagemovits <robertomovits@gmail.com>
2023-07-27 04:58:34 -0300

    arregle la funcion agregar

--- Ej3/script.js
+++ Ej3/script.js
@@ -1,12 +1,12 @@
 const inputImgs = document.querySelector("#input-imgs");
 const divImgs = document.querySelector("#div-imgs");
 
-const IMGS_AGREGADAS = inputImgs.value;
 const IMGS = [];
 
 function agregar() {
-    IMGS += IMGS_AGREGADAS;
+    const IMGS_AGREGADAS = inputImgs.value;
+    IMGS.push(IMGS_AGREGADAS);
     divImgs.innerHTML += `
-        <img src="${IMGS_AGREGADAS.value}" alt="">
+        <img src="${IMGS_AGREGADAS}" alt="">
     `;
 }
