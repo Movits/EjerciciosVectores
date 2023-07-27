1253ca7f90e1a4cd6a33fcc52dcf10837b7c64a5
Roberto Alagemovits <robertomovits@gmail.com>
2023-07-26 22:45:02 -0300

    arregle la funcion de compra

--- Ej2/script.js
+++ Ej2/script.js
@@ -1,13 +1,30 @@
 const Productos = ["Producto 1", "Producto 2", "Producto 3"];
 const Stock = [100,100,100];
+const prodSelect = document.querySelector("#producto-select");
+const prodDiv = document.querySelector("#tabla-productos");
 
 function compra() {
-    switch (key) {
-        case value:
-            
+    switch (prodSelect.value) {
+        case Productos[0]:
+            if (Stock[0] > 0) {
+                Stock[0] -= 1;
+            } 
             break;
-    
-        default:
+        case Productos[1]:
+            if (Stock[1] > 0) {
+                Stock[1] -= 1;
+            }
+            break;
+        case Productos[2]:
+            if (Stock[2] > 0) {
+                Stock[2] -= 1;
+            }
             break;
     }
+
+    prodDiv.innerHTML = `
+        <p id="producto1">Producto 1 - Stock: ${Stock[0]}</p>
+        <p id="producto2">Producto 2 - Stock: ${Stock[1]}</p>
+        <p id="producto3">Producto 3 - Stock: ${Stock[2]}</p>
+    `;
 }
